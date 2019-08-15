8728f49970227d774e3ceef8ac6b076fe8281006
vijayaganapathy <42263071+vijayaganapathy@users.noreply.github.com>
2019-08-15 07:05:55 -0700

    Create VowelorConsonant.c

---
+++ VowelorConsonant.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+    char char1;
+    scanf("%c",&char1);
+    switch(char1)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+        printf("Vowel");
+        break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'B':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+        case 'n':
+        case 'C':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+        case 'D':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+        printf("Consonant");
+        break;
+        default:
+        printf("invalid");
+    }
+    return 0;
+}
